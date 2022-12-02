6f4cfe5e450462df5e20542d4f44692d12efe71b
SanjanaNagwekar <sanjana.nagwekar@sjsu.edu>
2022-12-01 18:26:57 -0800

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -117,10 +117,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Print the current node first and then recurse on the children
 	public void preOrder() {
 		preOrderRecurse(root); 
+		System.out.println("PreOrder test commit");
 	}
 	
 	private void preOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null)
+            return;
+ 
+        System.out.print(node.data + " ");
+ 
+        preOrderRecurse(node.leftChild);
+ 
+        preOrderRecurse(node.rightChild);
 	}
 	
 	//Traverse the tree in an preorder fashion but using a stack
@@ -137,10 +145,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//For a bst this will print the values in sorted order from smallest to largest
 	public void inOrder() {
 		inOrderRecurse(root); 
+		System.out.println("InOrder test commit");
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null)
+            return;
+ 
+        inOrderRecurse(node.leftChild);
+ 
+        System.out.print(node.data + " ");
+ 
+        inOrderRecurse(node.rightChild);
 	}
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
@@ -153,10 +169,18 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	//Recurse on the children and then print the value in the current node
 	public void postOrder() {
 		postOrderRecurse(root); 
+		System.out.println("PostOrder test commit");
 	}
 	
 	public void postOrderRecurse(BSTNode<T> node) {
-		
+		if (node == null)
+            return;
+ 
+        postOrderRecurse(node.leftChild);
+ 
+        postOrderRecurse(node.rightChild);
+ 
+        System.out.print(node.data + " ");
 	}
 	
 	//Traverse the tree in an postorder fashion uses Stacks. 
@@ -221,8 +245,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
 		bst.postOrder();
 		System.out.println();
 		bst.postOrderStack();
-		
-		
 	}
 	
 
